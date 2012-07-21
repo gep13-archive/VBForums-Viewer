5e60ce1803b6255e41aec546f4b733df33699443
gep13 <gep13@gep13.co.uk>
2012-07-21 16:47:37 +0100

    Corrected StyleCop errors. Removed unneeded using statement in AppBootStrapper and temporarily moved instantiation on the Database into Caliburn.Micro container.

--- Gep13.WindowsPhone.VBForumsMetro.Database/IdentityTrigger.cs
+++ Gep13.WindowsPhone.VBForumsMetro.Database/IdentityTrigger.cs
@@ -1,4 +1,21 @@
-﻿namespace Gep13.WindowsPhone.VBForumsMetro.Database
+﻿//-----------------------------------------------------------------------
+// <copyright file="IdentityTrigger.cs" company="GEP13">
+//      Copyright (c) GEP13, 2012. All rights reserved.
+//      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+//      files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+//      modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+//      is furnished to do so, subject to the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+//      OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+//      LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+//      CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Gep13.WindowsPhone.VBForumsMetro.Database
 {
     using System.Linq;
 
@@ -7,10 +24,23 @@
     using Wintellect.Sterling;
     using Wintellect.Sterling.Database;
 
-    public class IdentityTrigger<T> : BaseSterlingTrigger<T, int> where T: class, IBaseModel, new()
+    /// <summary>
+    /// An integer based Identity which will be used to uniquely identify each object added to the database
+    /// </summary>
+    /// <typeparam name="T">The type that requires an identity int created</typeparam>
+    public class IdentityTrigger<T> : BaseSterlingTrigger<T, int> where T : class, IBaseModel, new()
     {
+        /// <summary>
+        /// Internal identifier used to keep track of the next Id to be allocated
+        /// </summary>
         private static int idx = 1;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityTrigger{T}"/> class. 
+        /// </summary>
+        /// <param name="database">
+        /// The instance of the Sterling Database that is to be assigned with an IdentityTrigger
+        /// </param>
         public IdentityTrigger(ISterlingDatabaseInstance database)
         {
             if (database.Query<T, int>().Any())
@@ -19,6 +49,11 @@
             }
         }
 
+        /// <summary>
+        /// See if we need to provide an id
+        /// </summary>
+        /// <param name="instance">Instance to check</param>
+        /// <returns>True if we're good to save</returns>
         public override bool BeforeSave(T instance)
         {
             if (instance.Id < 1)
@@ -29,11 +64,20 @@
             return true;
         }
 
+        /// <summary>
+        /// Simply return true, as no work is required here
+        /// </summary>
+        /// <param name="instance">Instance to check</param>
         public override void AfterSave(T instance)
         {
             return;
         }
 
+        /// <summary>
+        /// Simply return true, as no work is required here
+        /// </summary>
+        /// <param name="key">The Id of the key that needs checked</param>
+        /// /// <returns>True if we're good to delete</returns>
         public override bool BeforeDelete(int key)
         {
             return true;
--- Gep13.WindowsPhone.VBForumsMetro.Models/BaseModel.cs
+++ Gep13.WindowsPhone.VBForumsMetro.Models/BaseModel.cs
@@ -1,39 +1,60 @@
-﻿namespace Gep13.WindowsPhone.VBForumsMetro.Models
+﻿//-----------------------------------------------------------------------
+// <copyright file="BaseModel.cs" company="GEP13">
+//      Copyright (c) GEP13, 2012. All rights reserved.
+//      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+//      files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+//      modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+//      is furnished to do so, subject to the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+//      OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+//      LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+//      CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Gep13.WindowsPhone.VBForumsMetro.Models
 {
+    /// <summary>
+    /// The Base Class definition that all Models will derive from
+    /// </summary>
+    /// <typeparam name="T">The type that derives from base model</typeparam>
     public class BaseModel<T> : IBaseModel
         where T : BaseModel<T>
     {
         /// <summary>
-        /// Standard Key
+        /// Gets or sets the Standard Key
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// Make it easy for equality
+        /// Override the base Equals method and test for equality based on the objects Id property.
         /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
+        /// <param name="obj">The object being tested for equality</param>
+        /// <returns>True if the objects are equal, otherwise false.</returns>
         public override bool Equals(object obj)
         {
-            return obj is T && ((T)obj).Id.Equals(Id);
+            return obj is T && ((T)obj).Id.Equals(this.Id);
         }
 
         /// <summary>
-        /// 
+        /// Override the base GetHashCode method and return the Id for the current object
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The Id of the current object which will always be unique</returns>
         public override int GetHashCode()
         {
-            return Id;
+            return this.Id;
         }
 
         /// <summary>
-        /// 
+        /// Override the base ToString method to return sensible information about the current object.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A formatted string with information about the current object</returns>
         public override string ToString()
         {
-            return string.Format("{0} with Id {1}", typeof(T).FullName, Id);
+            return string.Format("{0} with Id {1}", typeof(T).FullName, this.Id);
         }
     }
 }
