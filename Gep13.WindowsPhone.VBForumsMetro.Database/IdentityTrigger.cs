737511e57e21317355e8e9eb8cda26d2cc6e9a81
gep13 <gep13@gep13.co.uk>
2012-07-21 16:19:40 +0100

    Added a new project to house all the logic for creating and maintainig the database for the VBForumsMetro application.

---
+++ Gep13.WindowsPhone.VBForumsMetro.Database/IdentityTrigger.cs
@@ -0,0 +1,42 @@
+﻿namespace Gep13.WindowsPhone.VBForumsMetro.Database
+{
+    using System.Linq;
+
+    using Gep13.WindowsPhone.VBForumsMetro.Models;
+
+    using Wintellect.Sterling;
+    using Wintellect.Sterling.Database;
+
+    public class IdentityTrigger<T> : BaseSterlingTrigger<T, int> where T: class, IBaseModel, new()
+    {
+        private static int idx = 1;
+
+        public IdentityTrigger(ISterlingDatabaseInstance database)
+        {
+            if (database.Query<T, int>().Any())
+            {
+                idx = database.Query<T, int>().Max(key => key.Key) + 1;
+            }
+        }
+
+        public override bool BeforeSave(T instance)
+        {
+            if (instance.Id < 1)
+            {
+                instance.Id = idx++;
+            }
+
+            return true;
+        }
+
+        public override void AfterSave(T instance)
+        {
+            return;
+        }
+
+        public override bool BeforeDelete(int key)
+        {
+            return true;
+        }
+    }
+}
